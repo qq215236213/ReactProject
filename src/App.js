bba169e7de889360da962a852fa4e74a125bb0b4
zhangleilei <asdfas@163.com>
2017-12-25 14:50:29 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
-import Home from './components/Home';
+import {Home,store} from './components/Home';
+import {Provider} from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-          <div>
-              <Route exact path='/' component={Home} />
-          </div>
-      </Router>
+       <Provider store={store}>
+          <Router>
+              <div>
+                  <Route exact path='/' component={Home} />
+              </div>
+          </Router>
+       </Provider>
     );
   }
 }
+
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,67 @@
-import React ,{Component} from 'react'
+import React ,{Component} from 'react';
+import {createStore} from 'redux';
+import  {connect} from 'react-redux'
 
 class Home extends Component{
     render(){
+        const {text,onChangeTxt,onBtnClick} = this.props;
         return (
             <div>
-                <h1>Home</h1>
+                <h1 onClick={onChangeTxt}>{text}</h1>
+                <button onClick={onBtnClick}>Click Me</button>
             </div>
         );
     }
 }
 
-export default Home;
+//action
+const changeTxtAction = {
+    type:'CHANGE_TEXT'
+}
+
+const buttonClickAction = {
+    type:'BUTTON_CLICK'
+}
+
+//reducer
+const initialState = {
+    text:'Hello'
+}
+
+const reducer = (state=initialState,action) =>{
+    switch (action.type){
+        case 'CHANGE_TEXT':
+            return {
+                text:state.text === 'Hello'?'World':'Hello'
+            }
+        case 'BUTTON_CLICK':
+            return {
+                text:'Hello World'
+            }
+        default:
+            return state;
+    }
+}
+
+
+//store
+let store = createStore(reducer);
+
+//映射Redux state到组件的属性
+function mapStateToProps(state) {
+    return {
+        text:state.text
+    }
+}
+
+//映射Redux actions 到组件的属性
+function mapDispatchToProps(dispath) {
+    return {
+		onChangeTxt:()=>dispath(changeTxtAction),
+		onBtnClick:()=>dispath(buttonClickAction)
+    }
+}
+
+Home = connect(mapStateToProps,mapDispatchToProps)(Home);
+
+export {Home,store};
