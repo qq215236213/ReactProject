603f583408fe22c3d8edc267341e433f92e38101
qq215236213 <215236213@qq.com>
2018-01-04 22:14:23 +0800

    test

---
+++ newreact/src/components/TestMenu.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from 'react';
+
+class TestMenu extends Component{
+    render(){
+        return (
+            <div>
+                <h1>haha</h1>
+            </div>
+        );
+    }
+}
+
+export default TestMenu;
